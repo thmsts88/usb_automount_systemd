4bbe12009a34f822648924e071145b563169bddd
Thomas <thomas.cuzziol@gmail.com>
2024-02-10 12:40:44 +0000

    initial commit

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+toJournal(){
+    echo $1 | systemd-cat  -t USBAutomountInstall -p info
+}
+
+toConsole(){
+    echo $1 
+}
+
+install(){
+    toJournal "Move mount_usb.sh to /usr/local/bin/"
+    toConsole "Move mount_usb.sh to /usr/local/bin/"
+    cp mount_usb.sh /usr/local/bin/mount_usb.sh
+    toJournal "Move mount_usb@.service to /etc/systemd/system/"
+    toConsole "Move mount_usb@.service to /etc/systemd/system/"
+    cp mount_usb@.service /etc/systemd/system/mount_usb@.service
+    toJournal "Move 99-mount_usb.rules to /etc/udev/rules.d/"
+    toConsole "Move 99-mount_usb.rules to /etc/udev/rules.d/"
+    cp 99-mount_usb.rules /etc/udev/rules.d/99-mount_usb.rules
+}
+
+install
